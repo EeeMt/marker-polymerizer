f4c1e2c8c449fde715b8889bd1052b5ded5058bd
eeemt <hxq@live.com>
2020-02-18 13:33:52 +0800

    add PlainMarker

---
+++ src/main/java/me/ihxq/projects/mp/model/PlainMarker.java
@@ -0,0 +1,13 @@
+package me.ihxq.projects.mp.model;
+
+import lombok.Data;
+
+/**
+ * @author xq.h
+ * 2020/2/16 17:38
+ **/
+@Data
+public class PlainMarker implements Marker {
+    private double x;
+    private double y;
+}
